0a6ae08540075cb587d4c8b2b279031acee7e417
Leiba-Viktoriya <victuuu0@gmail.com>
2026-05-29 00:45:55 +0500

    feat(#3): add CI pipeline with build, test, coverage > 90%

---
+++ SpaceBattle/SpaceBattle.Tests/CiBootstrapTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+using SpaceBattle.Lib;
+
+namespace SpaceBattle.Tests;
+
+public class CiBootstrapTests
+{
+    [Fact]
+    public void Ping_Returns_1()
+    {
+        Assert.Equal(1, CiBootstrap.Ping());
+    }
+}
